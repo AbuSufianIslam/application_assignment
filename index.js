d06ae1eb30ee6647519eaba122e02ab2683b06a4
AbuSufianIslam <thebooming1992@gmail.com>
2020-01-02 21:05:49 -0500

    Complete Solution

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const quotes = require('random-movie-quotes');
+const port = 3001;
 
 app.use(express.static('public'));
 
 app.get('/quote', (req, res) => {
-  res.json({
-    quote: quotes.getQuote()
-  });
+	res.json({
+		quote: quotes.getQuote()
+	});
 });
-
 
 app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
